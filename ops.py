f30c591e7c6c17f055824918ea9bf3a4fff2f8ec
unknown <profile2697@gmail.com>
2017-12-01 17:07:14 +0900

    add leaky relu

--- ops.py
+++ ops.py
@@ -3,6 +3,10 @@ import tensorflow as tf
 from data.fashion_mnist import IMAGE_PIXELS
 
 
+def leaky_relu(tensor, alpha=0.2):
+    return tf.maximum(alpha * tensor, tensor)
+
+
 def model_inputs(real_dim=IMAGE_PIXELS, z_dim=100):
     inputs_real = tf.placeholder(tf.float32, shape=[None, real_dim], name='inputs_real')
     inputs_z = tf.placeholder(tf.float32, shape=[None, z_dim], name='inputs_z')
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 import unittest
 
-from ops import model_inputs
+from ops import model_inputs, leaky_relu
 from data.fashion_mnist import IMAGE_PIXELS
 
 
-class TestLayers(unittest.TestCase):
+class TestModelInputs(unittest.TestCase):
     def setUp(self):
         tf.reset_default_graph()
 
@@ -21,3 +21,36 @@ class TestLayers(unittest.TestCase):
                           'Incorrect Image Shape.  Found {} shape'.format(inputs_z.get_shape().as_list()))
         self.assertEquals('inputs_z:0', inputs_z.name,
                           'Incorrect Name.  Found {}'.format(inputs_z.name))
+
+
+class TestLeakyRelu(unittest.TestCase):
+    def setUp(self):
+        tf.reset_default_graph()
+
+    def test_identity_when_input_is_bigger_than_zero(self):
+        inputs = tf.constant(1.)
+        leaky = leaky_relu(inputs, 0.2)
+
+        with tf.Session() as sess:
+            output = sess.run(leaky)
+            self.assertEquals(1., output,
+                              'Incorrect Value. Found {}'.format(output))
+
+    def test_identity_when_input_is_zero(self):
+        inputs = tf.constant(0.)
+        leaky = leaky_relu(inputs, 0.2)
+
+        with tf.Session() as sess:
+            output = sess.run(leaky)
+            self.assertEquals(0., output,
+                              'Incorrect Value. Found {}'.format(output))
+
+    def test_leak_when_input_is_smaller_than_zero(self):
+        inputs = tf.constant(-1.)
+        leaky = leaky_relu(inputs, 0.2)
+
+        with tf.Session() as sess:
+            output = sess.run(leaky)
+            self.assertAlmostEqual(-0.2, output,
+                                   delta=0.0001,
+                                   msg='Incorrect Value. Found {}'.format(output))
