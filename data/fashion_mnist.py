84fd5ebc2aac7a4a964838411e80ee2dad4f54ea
unknown <profile2697@gmail.com>
2017-12-01 14:01:23 +0900

    implemented batch data

--- data/fashion_mnist.py
+++ data/fashion_mnist.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
-import numpy as np
 
+# We Don't need to use test data
 DATA_PATH = os.path.join(os.path.dirname(__file__), '../dataset/fashionmnist/fashion-mnist_train.csv')
 
 IMAGE_SIZE = 28
@@ -9,10 +9,13 @@ IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
 
 
 class FashionMNIST:
-    def __init__(self):
-        self.raw_data = pd.read_csv(DATA_PATH)
-        self._images = self.raw_data.drop('label', axis=1).values
-        self._labels = self.raw_data['label'].values
+    def __init__(self, shuffle=True):
+        self.loaded_data = pd.read_csv(DATA_PATH)
+        if shuffle:
+            self.loaded_data = self.loaded_data.sample(frac=1).reset_index(drop=True)
+
+        self._images = self.loaded_data.drop('label', axis=1).values
+        self._labels = self.loaded_data['label'].values
 
     @property
     def images(self):
@@ -21,3 +24,18 @@ class FashionMNIST:
     @property
     def labels(self):
         return self._labels
+
+
+class DCGANFashionMNIST(FashionMNIST):
+    def __init__(self):
+        super().__init__()
+
+    def to_batches(self, batch_size=128):
+        batch_num = len(self.labels) // batch_size
+        for i in range(0, len(self.labels), batch_num):
+            yield self.images[i:i + batch_size]
+
+
+if __name__ == "__main__":
+    mnist = DCGANFashionMNIST()
+    batches = mnist.to_batches(batch_size=300)
