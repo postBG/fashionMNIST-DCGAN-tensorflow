94c32a9d1578972baadc84ea9f749ca4982545d6
unknown <profile2697@gmail.com>
2017-12-01 16:08:52 +0900

    add batch norm

--- ops.py
+++ ops.py
@@ -8,3 +8,7 @@ def model_inputs(real_dim=IMAGE_PIXELS, z_dim=100):
     inputs_z = tf.placeholder(tf.float32, shape=[None, z_dim], name='inputs_z')
 
     return inputs_real, inputs_z
+
+
+def batch_norm(inputs, training=True):
+    return tf.layers.batch_normalization(inputs, training=training)
