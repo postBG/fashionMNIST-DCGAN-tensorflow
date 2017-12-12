fba31e333214248f5a5e46abc3476276ad741ae1
unknown <profile2697@gmail.com>
2017-12-12 15:05:29 +0900

    implemented  DCGAN model

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import tensorflow as tf
 
+from data.fashion_mnist import IMAGE_SIZE
+from layers import model_inputs
+from models import DCGAN
+
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_integer("epoch", 10, "Epoch to train [10]")
 tf.app.flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
 tf.app.flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
+tf.app.flags.DEFINE_float("alpha", 0.2, "Alpha for leaky relu [0.2]")
 tf.app.flags.DEFINE_integer("batch_size", 32, "The size of batch images [32]")
+tf.app.flags.DEFINE_integer("kernel_size", 4, "The size of kernel [4]")
+tf.app.flags.DEFINE_integer("z_dim", 100, "The dimension of noise z [100]")
 
 
 def main(unused_args):
-    pass
+    inputs_real, inputs_z = model_inputs(IMAGE_SIZE, IMAGE_SIZE, 1, FLAGS.z_dim)
+    dcgan = DCGAN(inputs_real, inputs_z, FLAGS)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,8 +1,19 @@
 import tensorflow as tf
 
+from ops import model_loss
+from train import model_optimizer
+
 
 class DCGAN:
     def __init__(self, inputs_real, inputs_z, configs):
         self.inputs_real = inputs_real
         self.inputs_z = inputs_z
         self.configs = configs
+
+        self.d_loss, self.g_loss = model_loss(inputs_real, inputs_z,
+                                              output_channel=1,
+                                              kernel_size=self.configs.kernel_size,
+                                              alpha=self.configs.alpha)
+        self.d_optimizer, self.g_optimizer = model_optimizer(self.g_loss, self.d_loss,
+                                                   learning_rate=self.configs.learning_rate,
+                                                   beta1=self.configs.beta1)
